0a1f6c131ebd9357d6b65c7cb331e14b188b69c6
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-10 14:12:33 -0700

    [add] added templates folder and some files

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pprint import pprint
 app = Flask(__name__)
 
 @app.route("/") #Home Page #ROUTES are what we type in the browser into browser to go to different pages. We create these using ROUTE DECORATORS, which is a way to add additional functionalities to existing functions. This route decorator will handle all of the complicated backend stuff and simply allow us to write a function that returns the information that will be shown in our website
+@app.route("/home") #will access our home page via / or /home
 def home():
     return "Hello World"
 
@@ -18,4 +19,4 @@ def make_joke():
     return joke_str
     
 if __name__ == "__main__": #__name__ is main. But if we import this somewhere else, then the name will be the name of our module 
-    app.run(debug=True) #This conditional is only true if we run this script directly
+    app.run(debug=True) #This conditional is only true if we run this script directly 

3a869b8aa6a6b5935eacacd527da90daaf292a44
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-10 17:23:48 -0700

    [merged] with the starter code

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
+from flask import Flask, render_template, request
 import requests
-from flask import Flask
-from pprint import pprint
+import json
 
 app = Flask(__name__)
 
 @app.route("/") #Home Page #ROUTES are what we type in the browser into browser to go to different pages. We create these using ROUTE DECORATORS, which is a way to add additional functionalities to existing functions. This route decorator will handle all of the complicated backend stuff and simply allow us to write a function that returns the information that will be shown in our website
 @app.route("/home") #will access our home page via / or /home
-def home():
-    return "Hello World"
+def index():
+    """Return homepage."""
+    # TODO: Extract query term from url
 
-@app.route('/joke')
-def make_joke():
-    params = {"limitTo": ["nerdy"]} #create the parameters
-    r = requests.get("http://api.icndb.com/jokes/random",params=params)
-    joke_json = r.json() #conver
-    joke_str = joke_json["value"]
-    pprint(joke_str)
-    return joke_str
-    
+    # TODO: Make 'params' dict with query term and API key
+
+    # TODO: Make an API call to Tenor using the 'requests' library
+
+    # TODO: Get the first 10 results from the search results
+
+    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+
+    return render_template("index.html")
+
+
 if __name__ == "__main__": #__name__ is main. But if we import this somewhere else, then the name will be the name of our module 
     app.run(debug=True) #This conditional is only true if we run this script directly 

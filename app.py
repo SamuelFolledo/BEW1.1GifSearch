204942074682864e94d834cb12d25d02b56e1103
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-12 09:30:37 -0700

    [update]

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/gifs")
+def get_gif():
+
+    return render_template("gifs.html")
+
+
 @app.route("/") #Home Page #ROUTES are what we type in the browser into browser to go to different pages. We create these using ROUTE DECORATORS, which is a way to add additional functionalities to existing functions. This route decorator will handle all of the complicated backend stuff and simply allow us to write a function that returns the information that will be shown in our website
 @app.route("/home") #will access our home page via / or /home
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-
+    
     # TODO: Make 'params' dict with query term and API key
 
     # TODO: Make an API call to Tenor using the 'requests' library
@@ -17,6 +23,9 @@ def index():
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+
+    
+
 
     return render_template("index.html")
 

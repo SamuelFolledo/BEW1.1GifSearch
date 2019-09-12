3bf5e8dc7a2604de01ab74c61ed07cb857fbe2c5
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-12 13:23:25 -0700

    [add] added a way to get gif files from the Tenor api, and pprint the json file it retrieves

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from pprint import pprint #pretty print json
 
 app = Flask(__name__)
 
@@ -11,19 +12,33 @@ def index():
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
-    params = {
-        "q": query_term,
-        "Key": "18F7OZJBE0WJ"
-        }
+    
+    apiKey = "18F7OZJBE0WJ" #set the api key
+    limit = 10 #set limit
+    search_term = "excited" #test search
+
+    #get our gifs and applying our parameters
+    r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apiKey, limit))
+
+    
+    if r.status_code == 200:
+        #load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(r.content)
+        pprint(top_10gifs)
+        pprint
+    else:
+        top_10gifs = None
+
+    
+    
     # TODO: Make an API call to Tenor using the 'requests' library
-r = requests.get(
-    'https://tenor.com/gifapi/documentation',
-    params=params)
+    
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html")
-#added comment
+
+
 if __name__ == "__main__": #__name__ is main. But if we import this somewhere else, then the name will be the name of our module
     app.run(debug=True) #This conditional is only true if we run this script directly
